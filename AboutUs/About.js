f396331e629975c6c14da697030ea845edf8bd81
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-11-02 17:05:47 +0100

    added some animation and edited navbar

--- AboutUs/About.js
+++ AboutUs/About.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", () => {
   let nav = document.querySelector("header.first-section");
   if (window.scrollY > 50) {
     nav.classList.add("navbar-expand-lg-transparent-remove");
-  }else{
+  } else {
     nav.classList.remove("navbar-expand-lg-transparent-remove");
 
   }
@@ -15,15 +15,59 @@ window.addEventListener("scroll", () => {
 let click = document.querySelectorAll('.clickicon img');
 let media = document.querySelectorAll('.PersonSocialMedia .socialmediaicons');
 
-for(let i=0;i<click.length;i++){
-  click[i].addEventListener('click',function(){
-        if(media[i].style.display === "none"){
-          media[i].style.display="flex"
-        } else{
-          media[i].style.display="none"
-        }
+for (let i = 0; i < click.length; i++) {
+  click[i].addEventListener('click', function () {
+    if (media[i].style.display === "none") {
+      media[i].style.display = "flex"
+    } else {
+      media[i].style.display = "none"
+    }
   });
 }
-      
-   
-  
+
+// responsive image slides 
+
+
+
+  // window.addEventListener('scrol')
+  // let slideindex = 0;
+  // function showslide(){
+  //   let slides = document.querySelectorAll('.imagecards');
+
+  //   for(let i = 0; i < slides.length; i++){
+  //     slides[i].style.display="none"
+  //   }
+
+  //   slideindex++;
+
+  //   if(slideindex > slides.length){
+  //     slideindex=1;
+  //   }
+
+  //   slides[slideindex - 1].style.display="block"
+  //   setTimeout(showslide, 2000);
+  //  }
+  //  showslide();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
