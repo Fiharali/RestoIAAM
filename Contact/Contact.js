447b526af7c02d6791af582987de2988bea15c97
taouil abdo <abdotaouil03@gmail.com>
2023-11-03 12:19:46 +0100

    add navbar

--- Contact/Contact.js
+++ Contact/Contact.js
@@ -1,10 +1,22 @@
 window.addEventListener("scroll", () => {
-    let nav = document.querySelector("nav.navbar-expand-lg");
-    if (window.scrollY > 50) {
-      nav.classList.add("navbar-expand-lg-transparent-remove");
-    }else{
-      nav.classList.remove("navbar-expand-lg-transparent-remove");
-  
-    }
-  });
-  
+  let nav = document.querySelector("header.first-section");
+  if (window.scrollY > 50) {
+    nav.classList.add("navbar-expand-lg-transparent-remove");
+  } else {
+    nav.classList.remove("navbar-expand-lg-transparent-remove");
+
+  }
+});
+
+
+const validName = document.getElementById('input-Name');
+const validEmail = document.getElementById('input-Email');
+const validphoneNumber = document.getElementById('input-phoneNumber');
+const validNembers = document.getElementById('input-Nembers');
+const validMessage = document.getElementById('input-Message');
+
+const msgvalidName = document.getElementById('msg-valid-Name');
+const msgvalidEmail = document.getElementById('msg-valid-Email');
+const msgvalidphoneNumber = document.getElementById('input-phoneNumber');
+const msgvalidNembers = document.getElementById('input-Nembers');
+const msgvalidMessage = document.getElementById('input-Message');
